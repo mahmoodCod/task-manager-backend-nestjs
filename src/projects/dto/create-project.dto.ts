93e7981e16ad8bd851f3511d4303960406098a70
mahmoodCod <nmashoomy11@gmail.com>
2025-12-10 07:26:55 +0330

    feat: add length validation to project name
    - enforce min/max length (3-15) on CreateProjectDto.name
    - include validators MinLength/MaxLength alongside existing rules

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import { IsEnum, IsString } from 'class-validator';
+import { IsEnum, IsString, MaxLength, MinLength } from 'class-validator';
 import ProjectStatusEnum from '../enums/ProjectStatusEnum';
 
 export class CreateProjectDto {
   @IsString()
+  @MinLength(3)
+  @MaxLength(15)
   name: string;
 
   @IsEnum(ProjectStatusEnum)
